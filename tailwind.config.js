ce926bf29a5ad8c64ca6dee21808864170d99a32
Andoni Hernandez <andoni.hernandez.quero@gmail.com>
2023-08-15 01:32:45 +0200

    fix deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        'lg': 'shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]',
-      }
     },
   },
   plugins: [],
